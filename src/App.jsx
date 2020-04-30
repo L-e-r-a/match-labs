04de0a1da5a3da39ec6f822f54c733b8f2d6a863
Lera <lera.cernolev@gmail.com>
2020-04-30 11:06:22 +0300

    Week 2 - Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Profile from "./screens/Profile";
 import Likes from "./screens/Likes";
+import Library from "./screens/Library";
+import Toggle from "./components/Toggle";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toggle />
       <Switch>
         <Route path="/profile/:id" component={Profile}></Route>
+        <Route path="/library" component={Library}></Route>
         <Route path="/" component={Likes}></Route>
       </Switch>
     </BrowserRouter>
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import Card from "../components/Card";
 import styles from "./Profile.module.css";
-import { labs } from "../mocks";
 import Tags from "../components/Tags";
+import Card from "../components/Card";
+import { fetchProfile } from "../utils";
+import Loader from "../components/Loader";
 
 const Profile = (props) => {
-  const lab = labs.find((lab) => lab.id === parseInt(props.match.params.id));
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const onMount = async () => {
+      const id = props.match.params.id;
+      const profile = await fetchProfile(id);
+      setProfile(profile);
+    };
+    onMount();
+  }, []);
+
+  if (!profile) return <Loader />;
 
   return (
     <>
       <div className={styles.profile}>
         <div className={styles.hero}>
-          <Card imgUrl={lab.url}></Card>
+          <Card imgUrl={profile.profile_image}></Card>
         </div>
 
         <div className={styles.rightSide}>
-          <h3 className={styles.name}>{lab.name}</h3>
+          <h3 className={styles.name}>{profile.name}</h3>
 
           <section className={styles.skills}>
             <p className={styles.tagsTitle}>Technologies</p>
-            {lab.technologies && <Tags tags={lab.technologies}></Tags>}
+            {profile.technologies && <Tags tags={profile.technologies}></Tags>}
           </section>
 
           <section className={styles.objectives}>
             <h4 className={styles.heading}>Objectives</h4>
-            <p>{lab.objectives}</p>
+            <p>{profile.objectives}</p>
           </section>
 
           <section className={styles.description}>
             <h4 className={styles.heading}>About</h4>
-            {lab.description}
+
+            <p>{profile.description}</p>
           </section>
         </div>
       </div>
     </>
   );
 };
-
 export default Profile;
